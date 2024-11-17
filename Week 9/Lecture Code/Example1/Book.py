19785cbdafe4cb7e2fdcb54a92a43575c5330a38
Muath Othman <111856849+MuathOthman@users.noreply.github.com>
2024-11-17 19:43:17 +0200

    Delete Week 9 directory

--- Week 9/Lecture Code/Example1/Book.py
+++
@@ -1,8 +0,0 @@
-class Book:
-    def __init__(self, title, author, ibsn):
-        self.title = title
-        self.author = author
-        self.ibsn = ibsn
-
-    def display(self):
-        print("Book Title: " + self.title + " \nAuthor: " + self.author + " \nIBSN: " + str(self.ibsn))
